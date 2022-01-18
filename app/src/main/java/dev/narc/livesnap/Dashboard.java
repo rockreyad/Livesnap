a291981718367e61cf5bd363b806b8b1d36db405
Rockreyad <rockreyad@gmail.com>
2022-01-19 01:38:16 +0600

    initial update

--- app/src/main/java/dev/narc/livesnap/Dashboard.java
+++ app/src/main/java/dev/narc/livesnap/Dashboard.java
@@ -1,14 +1,30 @@
 package dev.narc.livesnap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Dashboard extends AppCompatActivity {
 
+    CardView profileBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        profileBtn = findViewById(R.id.profileBtn);
+
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,Profile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/dev/narc/livesnap/UserDetails.java
@@ -0,0 +1,14 @@
+package dev.narc.livesnap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_details);
+    }
+}
